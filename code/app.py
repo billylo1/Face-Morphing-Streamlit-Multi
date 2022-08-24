805d5cf2c11731945b06df3cbb3e34c207e6b2ba
Tim Conkling <tconkling@gmail.com>
2022-08-24 15:52:54 -0700

    Streamlit app

--- code/app.py
+++ code/app.py
@@ -1,4 +1,3 @@
-import argparse
 import pathlib
 import uuid
 
@@ -18,21 +17,6 @@ def doMorphing(img1, img2, duration, frame_rate, output):
     tri = make_delaunay(size[1], size[0], list3, img1, img2)
 
     generate_morph_sequence(duration, frame_rate, img1, img2, points1, points2, tri, size, output)
-
-
-def main() -> None:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--img1", required=True, help="The First Image")
-    parser.add_argument("--img2", required=True, help="The Second Image")
-    parser.add_argument("--duration", type=int, default=5, help="The duration")
-    parser.add_argument("--frame", type=int, default=20, help="The frameame Rate")
-    parser.add_argument("--output", help="Output Video Path")
-    args = parser.parse_args()
-
-    image1 = cv2.imread(args.img1)
-    image2 = cv2.imread(args.img2)
-
-    doMorphing(image1, image2, args.duration, args.frame, args.output)
 
 
 st.title("Face Morphing")
