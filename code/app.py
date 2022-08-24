77793c0a321ea13e22829033c41d0749d0bf2420
Tim Conkling <tconkling@gmail.com>
2022-08-24 16:12:42 -0700

    "Show Triangle Mesh" checkbox

--- code/app.py
+++ code/app.py
@@ -11,12 +11,10 @@ from face_morph import generate_morph_sequence
 IMAGES_DIR = pathlib.Path(__file__).parent.parent.joinpath("images/aligned_images").absolute()
 
 
-def doMorphing(img1, img2, duration, frame_rate, output):
+def doMorphing(img1, img2, duration: float, frame_rate: int, draw_triangles: bool, output: str) -> None:
     [size, img1, img2, points1, points2, list3] = generate_face_correspondences(img1, img2)
-
     tri = make_delaunay(size[1], size[0], list3, img1, img2)
-
-    generate_morph_sequence(duration, frame_rate, img1, img2, points1, points2, tri, size, output)
+    generate_morph_sequence(duration, frame_rate, img1, img2, points1, points2, tri, size, draw_triangles, output)
 
 
 st.title("Face Morphing")
@@ -32,6 +30,7 @@ with cols[1]:
 
 duration = st.number_input("Morph Duration", min_value=1.0, value=5.0)
 framerate = st.number_input("Morph Framerate", min_value=1, value=20)
+draw_triangles = st.checkbox("Show Triangle Mesh", value=False)
 
 if st.button("Morph!"):
     # Generate a filename
@@ -42,6 +41,7 @@ if st.button("Morph!"):
             img2=cv2.imread(str(image2_path)),
             duration=duration,
             frame_rate=framerate,
+            draw_triangles=draw_triangles,
             output=filename)
 
     st.video(filename)
--- code/face_morph.py
+++ code/face_morph.py
@@ -55,8 +55,18 @@ def morph_triangle(img1, img2, img, t1, t2, t, alpha) :
     img[r[1]:r[1]+r[3], r[0]:r[0]+r[2]] = img[r[1]:r[1]+r[3], r[0]:r[0]+r[2]] * ( 1 - mask ) + imgRect * mask
 
 
-def generate_morph_sequence(duration: float, frame_rate: int, img1, img2, points1, points2, tri_list, size, output: str) -> None:
-
+def generate_morph_sequence(
+    duration: float,
+    frame_rate: int,
+    img1,
+    img2,
+    points1,
+    points2,
+    tri_list,
+    size,
+    draw_triangles: bool,
+    output: str
+) -> None:
     num_images = int(duration*frame_rate)
     p = Popen(
         ['ffmpeg', '-y', '-f', 'image2pipe', '-r', str(frame_rate), '-s', str(size[1]) + 'x' + str(size[0]), '-i', '-',
@@ -98,9 +108,10 @@ def generate_morph_sequence(duration: float, frame_rate: int, img1, img2, points
             pt2 = (int(t[1][0]), int(t[1][1]))
             pt3 = (int(t[2][0]), int(t[2][1]))
 
-            cv2.line(morphed_frame, pt1, pt2, (255, 255, 255), 1, 8, 0)
-            cv2.line(morphed_frame, pt2, pt3, (255, 255, 255), 1, 8, 0)
-            cv2.line(morphed_frame, pt3, pt1, (255, 255, 255), 1, 8, 0)
+            if draw_triangles:
+                cv2.line(morphed_frame, pt1, pt2, (255, 255, 255), 1, 8, 0)
+                cv2.line(morphed_frame, pt2, pt3, (255, 255, 255), 1, 8, 0)
+                cv2.line(morphed_frame, pt3, pt1, (255, 255, 255), 1, 8, 0)
             
         res = Image.fromarray(cv2.cvtColor(np.uint8(morphed_frame), cv2.COLOR_BGR2RGB))
         res.save(p.stdin,'JPEG')
